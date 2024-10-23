a6c8c2eb1011d9749f0c7ba2644a68879224cd0d
rakhi singhal <rakhisinghal@zapbuild.com>
2024-10-23 11:52:48 +0530

    Final Listing done with all senarios

--- app/screens/Listing.tsx
+++ app/screens/Listing.tsx
@@ -17,7 +17,7 @@ export default function Listing() {
   }, []);
 
   const getData = async () => {
- 
+    setLoading(true)
       const url = "http://127.0.0.1:3000/user";
       const response = await fetch(url);
       if (!response.ok) {
@@ -25,22 +25,22 @@ export default function Listing() {
       }
       const result: Myapp[] = await response.json();
       setData(result);
-    
+    setLoading(false)
   };
 
-  // if (loading) {
-  //   return (
-  //     <View>
-  //       <ActivityIndicator size="large" color="#0000ff" />
-  //     </View>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
     <View>
       {data ? (
         data.map((itm) => (
-          <View key={itm.id}>
+          <View key={itm.id} style={{borderColor:'red',borderWidth:10,padding:5}}>
             <Text>{itm.name}</Text>
             <Text>{itm.age}</Text>
             <Text>{itm.email}</Text>
